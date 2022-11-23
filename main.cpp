4e068eba8a700dab0ebebf212ac8ffc994af5eee
sweavetheslayr <sweavertheslayr@outlook.com>
2022-11-23 17:31:28 -0500

    made level with ground and added koopa

--- main.cpp
+++ main.cpp
@@ -850,12 +850,11 @@ int main()
 		block.runningTime += window.dT;
 
 		//MAKE MOBS
-		
-		//goombas
 		for (int i = 0; i < 30; i++)
 		{
 			for (int j = (player.iPosX); j < window.blocksWide + window.renderPosX / window.blockHeight; j++)
 			{
+				//goombas
 				if (level.currentScene[i][j] == 'G')
 				{
 					level.currentScene[i][j] = ' ';
@@ -865,8 +864,20 @@ int main()
 					mob[window.mobCount].type = level.type;
 					window.mobCount += 1;
 				}
+				//koopas
+				else if (level.currentScene[i][j] == 'K')
+				{
+					level.currentScene[i][j] = ' ';
+					mob[window.mobCount].posX = (j)*window.blockHeight;
+					mob[window.mobCount].posY = (i - 4) * window.blockHeight;
+					mob[window.mobCount].mob = 3;
+					mob[window.mobCount].type = level.type;
+					window.mobCount += 1;
+				}
 			}
 		}
+
+
 
 		//MOB INFO
 		
@@ -1470,12 +1481,10 @@ int main()
 
 			for (int i = 0; i < window.mobCount; i++)
 			{
-				std::cout << mob[i].posX << " " << mob[i].posY << std::endl;
-
 				DrawTexturePro(
 					mob[i].texture,
-					Rectangle{ mob[i].mob * 16, (mob[i].frame * 32), 16, 32 },
-					Rectangle{ mob[i].posX - window.renderPosX, mob[i].posY, (32 * window.scale), (64 * window.scale) },
+					Rectangle{ mob[i].type * 16, (mob[i].mob * 32), 16, 32 },
+					Rectangle{ mob[i].posX - window.renderPosX, mob[i].posY - (8), (32 * window.scale), (64 * window.scale) },
 					Vector2{ 0, 0 },
 					0,
 					WHITE);
