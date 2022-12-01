50975880159c0c70435df9dbf39eb21104823cd9
sweavetheslayr <sweavertheslayr@outlook.com>
2022-12-01 09:25:03 -0500

    t5rwe3vt543rv453q

--- main.cpp
+++ main.cpp
@@ -1206,7 +1206,7 @@ int main()
 						{
 							Rectangle boxCollider{ mob[i].posX - (0.8 * window.blockHeight), mob[i].posY - ((3.4 + mob[i].stationary) * window.blockHeight) - 8, mob[i].width * window.scale * 1.2, mob[i].height * window.scale * 0.1 };
 							Rectangle playerCollider{ player.posX + window.renderPosX, player.posY + (!player.tall * 32), player.width * window.scale, player.height * window.scale };
-
+						
 							if (CheckCollisionRecs(boxCollider, playerCollider))
 							{
 								player.collision = true;
@@ -1215,7 +1215,6 @@ int main()
 					}
 				}
 
-				if ()
 
 				//other mobs
 				Rectangle boxCollider{ mob[i].posX - (1 * window.blockHeight), mob[i].posY - (4 * window.blockHeight) - 8, mob[i].width * window.scale * 2, mob[i].height * window.scale * 2 };
